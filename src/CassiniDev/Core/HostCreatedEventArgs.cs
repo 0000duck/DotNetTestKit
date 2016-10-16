d1f2f73b6aee316280e81d7e9fd2210a571f23a4
Mantas Indrašius <mantas@indrasius.lt>
2016-10-16 20:32:55 +0300

    HostCreatedEvent

---
+++ src/CassiniDev/Core/HostCreatedEventArgs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CassiniDev.Core
+{
+    public class HostCreatedEventArgs : EventArgs
+    {
+        private string physicalPath;
+        private string virtualPath;
+
+        public HostCreatedEventArgs(string virtualPath, string physicalPath)
+        {
+            this.virtualPath = virtualPath;
+            this.physicalPath = physicalPath;
+        }
+
+        public string PhysicalPath
+        {
+            get
+            {
+                return physicalPath;
+            }
+
+            set
+            {
+                physicalPath = value;
+            }
+        }
+
+        public string VirtualPath
+        {
+            get
+            {
+                return virtualPath;
+            }
+
+            set
+            {
+                virtualPath = value;
+            }
+        }
+    }
+}
